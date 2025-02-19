38e48a762717d5766f045e19d9793fb76f4ee369
Matheus011-tech <matheusaraujodasilva79@gmail.com>
2025-02-19 15:48:07 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,8 @@
 //Teste larissa
 
 int main() {
-    printf("Desafio Cartas Super Trunfo!\n")
+    printf("Desafio Cartas Super Trunfo!\n");
+    printf("Novo Commit\n");
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
